1e65810c50b09a25e88f243b1ca60f9951321c0a
Mert Oyman <mertoyman@gmail.com>
2016-11-08 21:17:56 +0200

    Migros stuff imported

---
+++ Migros/Assets/Scripts/AnswerSystem.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnswerSystem : MonoBehaviour {
+	public VideoLoader _videoLoader;
+
+	public string _toVideo;
+
+	[Header ("Answer Attr.")]
+	public int _questionID;
+	public int _answerID;
+	private int _packID;
+	private int _userID;
+
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ThisAnswerGiven(){
+	
+		_videoLoader._nextVideo = _toVideo;
+		_videoLoader.VideoLoad ();
+	
+		PlayerPrefs.SetString (string.Format ("{0}: {1:0}_ {2:0}: {3:0}_ {4:0}: {5:0}_ {6:0}: {7:0}", "user", _userID, "pack", _packID, "question", _questionID, "answer", _answerID), "Answer Given");
+
+		Debug.Log (string.Format ("{0}: {1:0}_ {2:0}: {3:0}_ {4:0}: {5:0}_ {6:0}: {7:0}", "user", _userID, "pack", _packID, "question", _questionID, "answer", _answerID));
+	}
+
+}
