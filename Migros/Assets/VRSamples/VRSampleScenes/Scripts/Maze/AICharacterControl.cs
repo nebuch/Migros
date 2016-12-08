7e7f11924c3cc00ef054a07ba8d59c84c53c3d1e
Mert Oyman <mertoyman@gmail.com>
2016-12-08 23:30:43 +0300

    Fixed main scene

--- Migros/Assets/VRSamples/VRSampleScenes/Scripts/Maze/AICharacterControl.cs
+++ Migros/Assets/VRSamples/VRSampleScenes/Scripts/Maze/AICharacterControl.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.AI;
 using VRStandardAssets.Maze;
 
 namespace VRStandardAssets.Maze
--- Migros/Assets/VRSamples/VRSampleScenes/Scripts/Maze/AgentTrail.cs
+++ Migros/Assets/VRSamples/VRSampleScenes/Scripts/Maze/AgentTrail.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace VRStandardAssets.Maze
 {
