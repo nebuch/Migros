183316e95e9cad0c2c6025279a7a581cfba826c1
nebuch <dogancihad@gmail.com>
2016-12-08 15:29:22 +0300

    PlayerPrefs and Intro to MainMenu action

---
+++ Migros/Assets/Scripts/dataRefresh.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class dataRefresh : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+		PlayerPrefs.DeleteAll ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
