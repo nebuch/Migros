fe3dffed95a6c1a60efa4f87eca71333519933db
nebuch <dogancihad@gmail.com>
2017-01-26 18:50:34 +0300

    thanks screen 1

---
+++ Migros/Assets/Scripts/Login/thanksCast.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class thanksCast : MonoBehaviour {
+
+	public Transform _newUser;
+	public Transform _newScenerio;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		RaycastHit hitted;
+		if (Physics.Raycast (transform.position, transform.forward, out hitted, 1000)) {
+			if (hitted.transform == _newUser) {
+
+				Debug.Log ("newUser");
+			}
+			if (hitted.transform == _newScenerio) {
+
+				Debug.Log ("newScenerio");
+			}
+		}
+			}
+
+
+	}
+
